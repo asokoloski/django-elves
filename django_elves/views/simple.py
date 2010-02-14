72f98b607f488347f9a67e40d08597ef5e0275ef
Aaron Sokoloski <aaron@unknown-d4-9a-20-6e-cd-04.home>
2010-02-14 22:14:04 +0000

    Fixed serve_rendered view to reset content-length of response, and delete Last-Modified header

--- django_elves/views/simple.py
+++ django_elves/views/simple.py
@@ -3,5 +3,8 @@ from django.template import Context, Template
 
 def serve_rendered(request, path, document_root=None, show_indexes=False):
     response = serve(request, path, document_root, show_indexes)
-    response.content = Template(response.content).render(Context({'request': request}))
+    contents = Template(response.content).render(Context({'request': request}))
+    response.content = contents
+    del response["Last-Modified"]
+    response["Content-Length"] = len(contents)
     return response

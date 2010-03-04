91131d9a479b9cb799740baaa4b8715264035dba
Aaron Sokoloski <asokoloski@gmail.com>
2010-03-04 12:04:56 +0000

    More verbose output for check()

--- django_elves/app_settings.py
+++ django_elves/app_settings.py
@@ -18,22 +18,32 @@ for name in ['ORIGINAL_PATH', 'OUTPUT_PATH', 'OUTPUT_URL', 'SPRITE_DEFS', 'COMPI
         globals()[name] = val()
 
 def check():
+    def ok(msg):
+        print msg
+
     if not COMPILED_PATH:
         raise ImproperlyConfigured('To use django-elves, you must set ELVES_COMPILED_PATH to an empty directory.  '
                                    'Compiled sprite definitions will be cached there.  This must be an absolute path.')
-
-    if not exists(ORIGINAL_PATH):
-        raise ImproperlyConfigured('ELVES_ORIGINAL_PATH (%r) does not exist.  It defaults to settings.MEDIA_ROOT.' % ORIGINAL_PATH)
-    if not exists(OUTPUT_PATH):
-        raise ImproperlyConfigured('ELVES_OUTPUT_PATH (%r) does not exist.  It defaults to settings.MEDIA_ROOT + "/autosprites".' % OUTPUT_PATH)
 
     if normpath(COMPILED_PATH) != normpath(abspath(COMPILED_PATH)):
         raise ImproperlyConfigured('ELVES_COMPILED_PATH must be an absolute path.  %r is invalid.' % COMPILED_PATH)
     if not exists(COMPILED_PATH):
         raise ImproperlyConfigured('ELVES_COMPILED_PATH (%r) does not exist.' % COMPILED_PATH)
+    ok('ELVES_COMPILED_PATH = %s' % COMPILED_PATH)
+
+    if not exists(ORIGINAL_PATH):
+        raise ImproperlyConfigured('ELVES_ORIGINAL_PATH (%r) does not exist.  It defaults to settings.MEDIA_ROOT.' % ORIGINAL_PATH)
+    ok('ELVES_ORIGINAL_PATH = %s' % ORIGINAL_PATH)
+
+    if not exists(OUTPUT_PATH):
+        raise ImproperlyConfigured('ELVES_OUTPUT_PATH (%r) does not exist.  It defaults to settings.MEDIA_ROOT + "/autosprites".' % OUTPUT_PATH)
+    ok('ELVES_OUTPUT_PATH = %s' % OUTPUT_PATH)
 
     try:
         __import__(SPRITE_DEFS, level=0)
     except ImportError, e:
         raise ImproperlyConfigured('Could not import ELVES_SPRITE_DEFS module (%r): %s' %
                                    (SPRITE_DEFS, e.args[0]))
+    ok('ELVES_SPRITE_DEFS = %s' % SPRITE_DEFS)
+
+    ok('ELVES_OUTPUT_URL = %s' % OUTPUT_URL)
